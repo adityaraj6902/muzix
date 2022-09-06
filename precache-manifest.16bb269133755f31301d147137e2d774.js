7d11c5d088599e00a99eab2f17b11f22b6754550
adityaraj6902 <adityaraj6902@gmail.com>
2022-09-07 02:18:40 +0530

    Updates

---
+++ precache-manifest.16bb269133755f31301d147137e2d774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "677386d54b91f322dc73",
+    "url": "/music/static/js/main.3179f723.chunk.js"
+  },
+  {
+    "revision": "3321ad820ee41c1cac90",
+    "url": "/music/static/js/runtime~main.4a84191b.js"
+  },
+  {
+    "revision": "ea5237aef22b4817d049",
+    "url": "/music/static/css/2.af03e7ea.chunk.css"
+  },
+  {
+    "revision": "ea5237aef22b4817d049",
+    "url": "/music/static/js/2.7a9d9c22.chunk.js"
+  },
+  {
+    "revision": "368928eaeb00d94c45d339f413d02bb2",
+    "url": "/music/index.html"
+  }
+];
